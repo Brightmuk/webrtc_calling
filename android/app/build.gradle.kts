f6fe7d27bde8911ddbc7f0839ce0ce1d7b4e7075
Bright Mukonesi <48471360+Brightmuk@users.noreply.github.com>
2025-10-16 21:23:49 +0300

    Initial commit

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "dev.flutterexplained.webrtc_tutorial"
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        
+        applicationId = "dev.flutterexplained.webrtc_tutorial"
+        minSdk = 23
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+dependencies {
+  implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
+  implementation("com.google.firebase:firebase-analytics")
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/dev/flutterexplained/webrtc_tutorial/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.flutterexplained.webrtc_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
